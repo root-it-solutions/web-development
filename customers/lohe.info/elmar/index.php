118222f027a5065fa3578d2b21f5f8754ac426af
elmar-root-it-solutions <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>
2024-11-26 10:03:08 +0100

    simple asset overview

--- customers/lohe.info/elmar/index.php
+++ customers/lohe.info/elmar/index.php
@@ -1,5 +1,18 @@
 <?php
 
+// remove header
+header_remove('ETag');
+header_remove('Pragma');
+header_remove('Cache-Control');
+header_remove('Last-Modified');
+header_remove('Expires');
+
+// set header
+header('Expires: Thu, 1 Jan 1970 00:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Cache-Control: post-check=0, pre-check=0',false);
+header('Pragma: no-cache');
+
 require "vendor/autoload.php";
 
 use Codenixsv\CoinGeckoApi\CoinGeckoClient;
