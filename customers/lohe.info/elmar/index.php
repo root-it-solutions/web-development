af25919320a7585028da5691611c6fd089c3ab89
Elmar <elmar@root-it.solutions>
2024-11-26 11:52:08 +0100

    simple asset overview

--- customers/lohe.info/elmar/index.php
+++ customers/lohe.info/elmar/index.php
@@ -76,7 +76,7 @@ foreach ($cgMappingCoinSymbol as $coin => $cgCoin)
     $tableContent_str .= '<tr><td>'.$coinUpper.'</td><td>'.number_format(${'amount' . $coinUpper}, 8).'</td><td>'.$eurValue.' &euro;</td></tr>';
 
 }
-$tableContent_str .= '<tr><td>Total</td><td></td><td>'.number_format($eurAmountTotal, 2, ',', '.').' &euro;</td></tr>';
+$tableContent_str .= '<tr class="total"><td>Total</td><td></td><td>'.number_format($eurAmountTotal, 2, ',', '.').' &euro;</td></tr>';
 //$body .= 'Total: ' . number_format($eurAmountTotal, 2, ',', '.') . ' EUR';
 //$body .= '<br />';
 //$body .= '</body></html>';
@@ -85,6 +85,6 @@ $path_to_file = 'main.html';
 $file_contents = file_get_contents($path_to_file);
 $file_contents = str_replace("{[[dataPointsArray]]}", $dataPointsArray_str, $file_contents);
 $file_contents = str_replace("{[[tableContent]}}", $tableContent_str, $file_contents);
-//$file_contents = str_replace("{[[body]]}", $body, $file_contents);
+$file_contents = str_replace("{[[body]]}", $body, $file_contents);
 echo $file_contents;
 //echo $body;
